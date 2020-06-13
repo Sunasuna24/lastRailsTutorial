8092d3ceb4cbd972976f310e8fdd1009661cad61
Tomoyasu Sunagawa <sunasunayaka1218@gmail.com>
2020-06-13 13:47:07 +0000

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,8 +10,3 @@ module ApplicationHelper
         end
     end
 end
-##test_should_get_home#StaticPagesControllerTest (0.30s)
-## =>   <Ruby on Rails Tutorial Sample App> expected but was
-## =>   <>Ruby on Rails Tutorial Sample App>..
-## =>   Expected 0 to be >= 1.
-## =>   test/controllers/static_pages_controller_test.rb:8:in `block in <class:StaticPagesControllerTest>'
